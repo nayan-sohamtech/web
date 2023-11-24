07c81c1d581c6880692a10c4e1e3dd59febb6ea1
nayan-sohamtech <148191903+nayan-sohamtech@users.noreply.github.com>
2023-11-24 17:54:57 +0530

    second commit

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -38,7 +38,7 @@ AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
               style: Theme.of(context).textTheme.titleLarge,
             ),
           ),
-          // if (!ResponsiveWidget.isSmallScreen(context))
+          //if (!ResponsiveWidget.isSmallScreen(context))
           Spacer(flex: ResponsiveWidget.isLargeScreen(context) ? 2 : 1),
           const Expanded(
             child: SearchField(),
@@ -79,28 +79,98 @@ class ProfileCard extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: defaultPadding / 2),
               child: Text("Angelina Joli"),
             ),
-          PopupMenuButton(
-            color: bgColor,
+          PopUpMenu(
             icon: const Icon(
-              Icons.keyboard_arrow_down,
+              Icons.keyboard_arrow_down_sharp,
               color: Colors.white,
             ),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            splashRadius: 1,
-            offset: Offset(0.0, appBarHeight),
-            itemBuilder: (context) => [
-              _popupMenuItem(
-                  "Profile", Icons.account_box, Options.profile.index),
-              _popupMenuItem("Email", Icons.email, Options.email.index),
-              _popupMenuItem(
-                  "Settings", Icons.settings, Options.settings.index),
+            menuList: [
+              const PopupMenuItem(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: primaryColor,
+                    child: Text(
+                      "n",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                    ),
+                  ),
+                  title: Text(
+                    "Nayan Moradiya",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  subtitle: Text(
+                    "abcd@gmail.com",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const PopupMenuDivider(),
+              PopupMenuItem(
+                child: ListTile(
+                  splashColor: bgColor,
+                  leading: const Icon(Icons.person_sharp),
+                  title: const Text("Profile"),
+                  onTap: () {},
+                ),
+              ),
+              const PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.g_mobiledata_sharp),
+                  title: Text("Google Account"),
+                ),
+              ),
+              const PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.switch_account_sharp),
+                  title: Text("Account"),
+                  trailing: Icon(Icons.keyboard_arrow_right_sharp),
+                ),
+              ),
             ],
           ),
-          //const Icon(Icons.keyboard_arrow_down),
+          // PopupMenuButton(
+          //   color: primaryColor,
+          //   icon: const Icon(
+          //     Icons.keyboard_arrow_down,
+          //     color: Colors.white,
+          //   ),
+          //   shape: const RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.all(Radius.circular(10)),
+          //   ),
+          //   splashRadius: 1,
+          //   offset: Offset(20.0, appBarHeight),
+          //   itemBuilder: (context) => [
+          //     PopupMenuItem(
+          //       child: ListTile(
+          //         leading: CircleAvatar(
+          //           backgroundColor: bgColor,
+          //         ),
+          //         title: Text(
+          //           "NM",
+          //         ),
+          //       ),
+          //     ),
+
+          // UserAccountsDrawerHeader(
+          //   accountName: Text("Nayan Moradiya"),
+          //   accountEmail: Text("Abcd@gmail.com"),
+          //   currentAccountPicture: CircleAvatar(
+          //     backgroundColor: primaryColor,
+          //     child: Text(
+          //       "NM",
+          //       style: TextStyle(color: Colors.white),
+          //     ),
+          //   ),
+          // ),
+
+          // _popupMenuItem(
+          //     "Profile", Icons.account_box, Options.profile.index),
+          // _popupMenuItem("Email", Icons.email, Options.email.index),
+          // _popupMenuItem(
+          //     "Settings", Icons.settings, Options.settings.index),
         ],
       ),
+      //const Icon(Icons.keyboard_arrow_down),
     );
   }
 }
@@ -145,10 +215,34 @@ class SearchField extends StatelessWidget {
   }
 }
 
-PopupMenuItem _popupMenuItem(String title, IconData iconData, int position) {
-  return PopupMenuItem(
-    child: Row(
-      children: [Text(title)],
-    ),
-  );
+class PopUpMenu extends StatelessWidget {
+  final List<PopupMenuEntry> menuList;
+  final Widget? icon;
+  const PopUpMenu({Key? key, required this.menuList, this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      color: bgColor,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      splashRadius: 1,
+      offset: Offset(17.0, appBarHeight),
+      itemBuilder: ((context) => menuList),
+      icon: icon,
+    );
+  }
 }
+// PopupMenuItem _popupMenuItem() {
+//   return PopupMenuItem(
+//     child: Row(
+//       children: [
+//         ListTile(
+//           leading: Icon(),
+//         )
+//       ],
+//     ),
+//   );
+// }
